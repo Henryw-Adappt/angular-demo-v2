ff3dcfa81541fdbb45078f1f9bbb125ccd938474
Henry Winchester <130577603+Henryw-Adappt@users.noreply.github.com>
2024-03-21 16:48:30 +0000

    Observables in service

--- src/app/blog/likes.service.ts
+++ src/app/blog/likes.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class LikesService {
+export class LikesService implements OnDestroy {
+  private _destroy$ = new Subject<boolean>();
+
+  private _liked$ = new Subject<number>();
+
   private _likes = 0;
   public get likes() {
     return this._likes;
@@ -12,8 +17,33 @@ export class LikesService {
     this._likes = likes;
   }
 
+  constructor() {
+    // fromEvent(HTMLElement, 'click').pipe().subscribe;
+    // from(new Promise())
+    // const likedObs = this._liked$.pipe(
+    //   takeUntil(this._destroy$),
+    //   map(likes => `Likes: ${likes}`),
+    //   debounce(() => timer(100))
+    // );
+    // likedObs.subscribe({
+    //   next: likes => console.log(likes),
+    //   complete: () => console.log('complete'),
+    //   error: () => console.error('error'),
+    // });
+    // likedObs.subscribe({
+    //   next: likes => console.log(likes),
+    //   complete: () => console.log('complete'),
+    //   error: () => console.error('error'),
+    // });
+  }
+
   public like() {
-    console.log('like');
     this._likes++;
+    this._liked$.next(this._likes);
+  }
+
+  public ngOnDestroy(): void {
+    this._destroy$.next(true);
+    this._destroy$.complete();
   }
 }
